efa94f770b0b88e03210771285c8cb269910ef17
Juan de Dios Torres <juand_cris@hotmail.com>
2016-04-24 23:54:11 -0500

    Vista preliminar bluetooth

--- app/src/main/java/com/example/juand/cleverhome/MainActivity.java
+++ app/src/main/java/com/example/juand/cleverhome/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.juand.cleverhome;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        TabHost tabHost = (TabHost) findViewById(R.id.tabHost);
+        tabHost.setup();
+
+        TabHost.TabSpec tabSpec = tabHost.newTabSpec("bluetooth");
+        tabSpec.setContent(R.id.bluetoothTab);
+        tabSpec.setIndicator("Bluetooth");
+
+        tabHost.addTab(tabSpec);
+
+        tabSpec = tabHost.newTabSpec("ethernet");
+        tabSpec.setContent(R.id.ethernetTab);
+        tabSpec.setIndicator("Ethernet");
+
+        tabHost.addTab(tabSpec);
     }
 }
